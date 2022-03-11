a431848fafa02e7ca904777ae28e2686b69793f6
Adam <40492256@live.napier.ac.uk>
2022-03-11 10:17:15 +0000

    Interacting with Window, Lab 8

---
+++ Labs/Lab 8 - Javascript Client/js_functions.js
@@ -0,0 +1,18 @@
+function addMessage()
+{
+    document.getElementById("redPara").style.color = "blue";
+}
+
+var current="red";
+
+function colourChanger()
+{
+    if(current === "red")
+        current = "blue";
+                    
+    else
+        current = "red";
+                    
+    document.getElementById("redPara").style.color = current;
+    document.getElementById("redPara").innerHTML = "I am " + current; 
+}
